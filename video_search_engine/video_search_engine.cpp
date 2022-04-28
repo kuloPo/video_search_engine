e4fd3a80f985a633e7afbc7a3f5ce481d7401573
クロポ <reg@kulopo.com>
2022-04-27 22:12:40 -0700

    use std thread for multi threading

--- video_search_engine/video_search_engine.cpp
+++ video_search_engine/video_search_engine.cpp
@@ -1,3 +1,5 @@
+#include <queue>
+
 #include "similar.h"
 #include "algo.h"
 #include "io.h"
@@ -5,49 +7,73 @@
 
 std::mutex stdout_mutex;
 std::mutex db_mutex;
+std::unique_ptr<DB_Connector> DB;
 
-int main() {
-	std::unique_ptr<DB_Connector> DB = init_db();
-	std::vector<std::filesystem::path> queue;
-	for (const auto& entry : std::filesystem::directory_iterator(video_path)) {
-		queue.push_back(entry.path().filename());
+std::queue<std::filesystem::path> working_queue;
+std::mutex queue_mutex;
+
+void build_index(std::filesystem::path filename) {
+	// check if already exist in db
+	std::string ID;
+	picosha2::hash256_hex_string(filename.string(), ID);
+	std::string search_sql = form_search_sql(ID);
+	std::unique_ptr<pqxx::result>& query_result = DB->performQuery(search_sql);
+	if (!query_result->empty()) {
+		safe_printf("%s Already indexed\n", filename.string().c_str());
+		return;
 	}
 
-	parallel_for_(cv::Range(0, queue.size()), [&](const cv::Range& range) {
-		for (int i = range.start; i < range.end; i++) {
-			std::filesystem::path filename = queue[i];
-			safe_printf("%s start\n", filename.string().c_str());
-			// check if already exist in db
-			std::string ID;
-			picosha2::hash256_hex_string(filename.string(), ID);
-			std::string search_sql = form_search_sql(ID);
-			std::unique_ptr<pqxx::result>& query_result = DB->performQuery(search_sql);
-			if (!query_result->empty()) {
-				safe_printf("%s Already indexed\n", filename.string().c_str());
-				continue;
-			}
+	// extract key frames
+	std::vector<Key_Frame*> key_frames = std::move(create_index(video_path / filename));
+	// get interval
+	int fps = get_fps(video_path / filename);
+	std::vector<int> interval, interval_merged;
+	calc_interval(key_frames, interval);
+	interval_merge(interval, fps, interval_merged);
+	// write interval to database
+	std::string interval_str = write_interval(interval_merged, filename);
+	std::string filename_str = filename.string();
+	filename_str = std::regex_replace(filename_str, std::regex("'"), "''");
+	std::string insert_sql = form_insert_sql(ID, filename_str, fps, interval_str);
+	DB->performQuery(insert_sql);
+	// write frame image to disk
+	//write_key_frame(key_frames, index_path, filename);
+	// clean up
+	for (Key_Frame* key_frame : key_frames) {
+		delete key_frame;
+	}
+}
 
-			// extract key frames
-			std::vector<Key_Frame*> key_frames = std::move(create_index(video_path / filename));
-			// get interval
-			int fps = get_fps(video_path / filename);
-			std::vector<int> interval, interval_merged;
-			calc_interval(key_frames, interval);
-			interval_merge(interval, fps, interval_merged);
-			// write interval to database
-			std::string interval_str = write_interval(interval_merged, filename);
-			std::string filename_str = filename.string();
-			filename_str = std::regex_replace(filename_str, std::regex("'"), "''");
-			std::string insert_sql = form_insert_sql(ID, filename_str, fps, interval_str);
-			DB->performQuery(insert_sql);
-			// write frame image to disk
-			//write_key_frame(key_frames, index_path, filename);
-			// clean up
-			for (Key_Frame* key_frame : key_frames) {
-				delete key_frame;
-			}	
-		}
-	}, 1);
+void thread_invoker(int deviceID) {
+#ifdef HAVE_OPENCV_CUDACODEC
+	cv::cuda::setDevice(deviceID);
+#endif
+	while (working_queue.empty() == false) {
+		queue_mutex.lock();
+		std::filesystem::path filename = working_queue.front();
+		working_queue.pop();
+		queue_mutex.unlock();
+		safe_printf("%s start. thread: %d\n", filename.string().c_str(), deviceID);
+		build_index(filename);
+	}
+}
+
+int main() {
+	DB = init_db();
 	
+	for (const auto& entry : std::filesystem::directory_iterator(video_path)) {
+		working_queue.push(entry.path().filename());
+	}
+	
+	std::vector<std::thread> thread_list;
+	for (int i = 0; i < thread_num; i++) {
+		std::thread t(thread_invoker, i);
+		thread_list.push_back(std::move(t));
+	}
+	
+	for (auto iter = thread_list.begin(); iter != thread_list.end(); iter++) {
+		iter->join();
+	}
+
 	return 0;
 }
