9fb869ac4dc01a2d193b67ff445bb2c9b54c2279
クロポ <reg@kulopo.com>
2022-05-12 21:10:47 -0700

    Revert "improve interval comparison"
    This reverts commit 1f52ac9f6b7c4093a8d110efae625a88c00fd22b.

--- src/similar.cpp
+++ src/similar.cpp
@@ -18,130 +18,6 @@
 
 #include "similar.h"
 
-Interval_Comparison::Interval_Comparison(const std::vector<double>& interval1, const std::vector<double>& interval2) {
-	this->interval1 = interval1;
-	this->interval2 = interval2;
-	this->matched1 = std::vector<Status>(interval1.size(), Status::NOT_MATCHED);
-	this->matched2 = std::vector<Status>(interval2.size(), Status::NOT_MATCHED);
-	this->main_p = cv::Vec2i(0, 0);
-}
-
-cv::Vec2i Interval_Comparison::find_direct_match() {
-	for (int i = this->main_p[0]; i < this->interval1.size(); i++) {
-		for (int j = this->main_p[1]; j < this->interval2.size(); j++) {
-			if (abs(interval1[i] - interval2[j]) <= 0.5 &&
-				interval1[i] >= 10 and interval2[j] >= 10) {
-				return cv::Vec2i(i, j);
-			}
-		}
-	}
-	return cv::Vec2i(-1, -1);
-}
-
-bool Interval_Comparison::expand(Direction direction) {
-	double sum1, sum2, increment;
-	sum1 = sum2 = 0;
-	while (abs(sum1 - sum2) > 0.5 || sum1 == 0 && sum2 == 0) {
-		if (sum1 < sum2 || sum1 == 0 && sum2 == 0) {
-			increment = this->explore(this->interval1, this->matched1, this->main_p[0], direction);
-			if (increment == -1) {
-				return false;
-			}
-			sum1 += increment;
-			this->main_p[0] += direction;
-		}
-		else {
-			increment = this->explore(this->interval2, this->matched2, this->main_p[1], direction);
-			if (increment == -1) {
-				return false;
-			}
-			sum2 += increment;
-			this->main_p[1] += direction;
-		}
-	}
-	this->mark(this->matched1, this->main_p[0], direction);
-	this->mark(this->matched2, this->main_p[1], direction);
-	return true;
-}
-
-double Interval_Comparison::explore(
-	const std::vector<double>& interval,
-	const std::vector<Status>& matched,
-	int p, Direction direction) const
-{
-	if (direction == Direction::LEFT && p == 0) {
-		return -1;
-	}
-	if (direction == Direction::RIGHT && p == interval.size() - 1) {
-		return -1;
-	}
-	if (matched[p + direction] != Status::NOT_MATCHED) {
-		return -1;
-	}
-	return interval[p + direction];
-}
-
-void Interval_Comparison::mark(std::vector<Status>& matched, int p, Direction direction) {
-	matched[p] = Status::EXPAND_MATCH_BOUNDARY;
-	p += direction * -1;
-	while (matched[p] != Status::DIRECT_MATCH && matched[p] != Status::EXPAND_MATCH_BOUNDARY) {
-		matched[p] = Status::EXPAND_MATCH;
-		p += direction * -1;
-	}
-}
-
-void Interval_Comparison::compare() {
-	this->main_p = this->find_direct_match();
-	while (this->main_p != cv::Vec2i(-1, -1)) {
-		this->matched1[this->main_p[0]] = Status::DIRECT_MATCH;
-		this->matched2[this->main_p[1]] = Status::DIRECT_MATCH;
-		while (this->expand(Direction::LEFT));
-		while (this->matched1[this->main_p[0]] != Status::DIRECT_MATCH) {
-			this->main_p[0]++;
-		}
-		while (this->matched2[this->main_p[1]] != Status::DIRECT_MATCH) {
-			this->main_p[1]++;
-		}
-		while (this->expand(Direction::RIGHT));
-		while (this->matched1[this->main_p[0]] == Status::NOT_MATCHED) {
-			this->main_p[0]--;
-		}
-		while (this->matched2[this->main_p[1]] == Status::NOT_MATCHED) {
-			this->main_p[1]--;
-		}
-		this->main_p[0]++;
-		this->main_p[1]++;
-		this->main_p = this->find_direct_match();
-	}
-}
-
-int Interval_Comparison::get_matched() const {
-	int sum = 0;
-	for (Status i : this->matched1) {
-		if (i != Status::NOT_MATCHED) {
-			sum++;
-		}
-	}
-	return sum;
-}
-
-void Interval_Comparison::print_matched() const {
-	cout << "interval 1: \n";
-	for (int i = 0; i < this->interval1.size(); i++) {
-		double interval = this->interval1[i];
-		Status status = this->matched1[i];
-		printf("\x1B[%dm%.2f\033[0m ", 31 + status, interval);
-	}
-	cout << endl;
-	cout << "interval 2: \n";
-	for (int i = 0; i < this->interval2.size(); i++) {
-		double interval = this->interval2[i];
-		Status status = this->matched2[i];
-		printf("\x1B[%dm%.2f\033[0m ", 31 + status, interval);
-	}
-	cout << endl;
-}
-
 #ifdef HAVE_OPENCV_CUDACODEC
 cv::cuda::GpuMat get_histogram(const cv::cuda::GpuMat img) {
 	cv::cuda::GpuMat hist;
@@ -272,12 +148,140 @@ cv::Vec2i _path_back_trace(const cv::Mat& m, const cv::Mat& path_trace, const cv
 }
 
 int interval_comparison(const std::vector<double>& v1, const std::vector<double>& v2) {
-	Interval_Comparison comp(v1, v2);
-	comp.compare();
+	// use dynamic programming to find the number of matched interval
+	int n1 = v1.size();
+	int n2 = v2.size();
+	cv::Mat m = cv::Mat::zeros(n1 + 1, n2 + 1, CV_32SC1);
+	cv::Mat path_trace = cv::Mat::zeros(n1 + 1, n2 + 1, CV_32SC2);
+
+	for (int x = 1; x <= n1; x++) {
+		for (int y = 1; y <= n2; y++) {
+			if (cv::abs(v1[x - 1] - v2[y - 1]) < interval_matching_epsilon) { // difference smaller than epsilon, match found 
+				m.at<int>(x, y) = m.at<int>(x - 1, y - 1) + 1; // update value
+				// if this is the first match found on the same level, record its coordinate
+				if (m.at<int>(x, y) > m.at<int>(x - 1, y) && m.at<int>(x, y) > m.at<int>(x, y - 1)) {
+					path_trace.at<cv::Vec2i>(x, y) = cv::Vec2i(x, y);
+				}
+				else {
+					path_trace.at<cv::Vec2i>(x, y) = _path_back_trace(m, path_trace, cv::Vec2i(x, y));
+				}
+			}
+			else { // not match, inherit value 
+				m.at<int>(x, y) = max_3(m.at<int>(x - 1, y - 1), m.at<int>(x, y - 1), m.at<int>(x - 1, y));
+				path_trace.at<cv::Vec2i>(x, y) = _path_back_trace(m, path_trace, cv::Vec2i(x, y));
+			}
+		}
+	}
+
+	// coordinates representing the index of matched intervals
+	std::vector<cv::Vec2i> paths;
+	// start tracing from button right corner of mat
+	cv::Vec2i last_path = path_trace.at<cv::Vec2i>(n1, n2);
+	paths.push_back(cv::Vec2i(n1 + 1, n2 + 1));
+	while (last_path != cv::Vec2i(0, 0)) {
+		paths.push_back(last_path);
+		last_path = _path_back_trace(m, path_trace, last_path);
+	}
+
+	// break intervals at where they are matched
+	// sum up unmatched intervals in between and check if are equal
+	// interval 1: 1 | 2 | 3       | 4 | 2.5 2.5 | 6
+	// interval 2: 1 | 2 | 1.5 1.5 | 4 | 5       | 6
+	std::vector<int> matched_index;
+
+	int last_1, last_2;
+	last_1 = last_2 = 0;
+	while (!paths.empty()) {
+		cv::Vec2i path = paths.back();
+		paths.pop_back();
+		int x = path[0] - 1;
+		int y = path[1] - 1;
 #ifdef DEBUG_INTERVAL_COMPARISON
-	comp.print_matched();
-#endif
-	return comp.get_matched();
+		if (!paths.empty()) {
+			printf("%d: %.2f, %d: %.2f\n", x, v1[x], y, v2[y]);
+		}
+#endif // DEBUG_INTERVAL_COMPARISON
+		if (x - last_1 > 10 || y - last_2 > 10) {
+			last_1 = x;
+			last_2 = y;
+			continue;
+		}
+		double sum_1, sum_2;
+		sum_1 = sum_2 = 0;
+		for (int i = last_1; i < x; i++) {
+			sum_1 += v1[i];
+		}
+		for (int i = last_2; i < y; i++) {
+			sum_2 += v2[i];
+		}
+		// prevent one very large interval happens to match the other
+		// normal interval    : 1 2 3 4
+		// very large interval: 1 ... 2 ... 3 ... 4
+		if (cv::abs(sum_1 - sum_2) < interval_matching_epsilon) {
+			for (int i = last_1; i <= x; i++) {
+				if (i < v1.size() && (matched_index.empty() || i != matched_index.back())) {
+					matched_index.push_back(i);
+				}
+			}
+		}
+		last_1 = x;
+		last_2 = y;
+	}
+
+	// find consecutive sequence greater than threshold
+	int sum, last, consecutive_count;
+	sum = 0;
+	last = -1;
+	consecutive_count = 0;
+	for (int i : matched_index) {
+		if (last == -1) {
+			consecutive_count = 1;
+		}
+		else if (i == last + 1) {
+			consecutive_count++;
+		}
+		else {
+			if (consecutive_count >= min_matched_interval) {
+				sum += consecutive_count;
+			}
+			consecutive_count = 1;
+		}
+		last = i;
+	}
+	if (consecutive_count >= min_matched_interval) {
+		sum += consecutive_count;
+	}
+
+#ifdef DEBUG_INTERVAL_COMPARISON
+	cout << endl << endl;
+	for (int i : matched_index) {
+		cout << i << " ";
+	}
+	cout << endl << endl;
+
+	for (int x = 1; x <= n1; x++) {
+		for (int y = 1; y <= n2; y++) {
+			if (m.at<int>(x, y) >= 0) {
+				std::cout << m.at<int>(x, y) << " ";
+			}
+			else {
+				std::cout << "  ";
+			}
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+
+	for (int x = 1; x <= n1; x++) {
+		for (int y = 1; y <= n2; y++) {
+			printf("(%d,%d) ", path_trace.at<cv::Vec2i>(x, y)[0], path_trace.at<cv::Vec2i>(x, y)[1]);
+		}
+		std::cout << std::endl;
+	}
+#endif // DEBUG_INTERVAL_COMPARISON
+
+	return sum;
+	//return matched_index.size();
 }
 
 double radon_distance(const cv::Mat& radon1, const cv::Mat& radon2) {
