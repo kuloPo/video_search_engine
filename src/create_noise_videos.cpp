2b6c949e0e1b0b8ac1fa818abd8387cd8e0b4124
クロポ <reg@kulopo.com>
2022-12-24 07:32:58 -0800

    update create_noise_video

--- src/create_noise_videos.cpp
+++ src/create_noise_videos.cpp
@@ -39,42 +39,37 @@ std::string seed_prefix;
 std::queue<std::filesystem::path> working_queue;
 std::mutex queue_mutex;
 
-void create_video(const std::filesystem::path& video_path, const double SNR) {
-    std::string hash;
-    hash_string(video_path.string(), hash);
-    std::string seed = seed_prefix + hash.substr(0, 15);
-    cv::theRNG().state = std::stoull(seed, nullptr, 16);
+class Noise_Video_Writer : public Video_Reader {
+public:
+    Noise_Video_Writer(const std::filesystem::path& video_path, const double SNR) : Video_Reader(video_path) {
+        this->SNR = SNR;
+        this->output_path = output_dir / this->filename.filename();
+        this->output_path.replace_extension("avi");
 
-    std::filesystem::path output_path = output_dir / video_path.filename();
-    output_path.replace_extension("avi");
+        std::string hash;
+        hash_string(this->filename.filename().string(), hash);
+        std::string seed = seed_prefix + hash.substr(0, 15);
+        cv::theRNG().state = std::stoull(seed, nullptr, 16);
 
-    cv::Mat frame;
-#ifdef HAVE_OPENCV_CUDACODEC
-    cv::Ptr<cv::cudacodec::VideoReader> video_reader = cv::cudacodec::createVideoReader(video_path.string());
-    cv::cuda::GpuMat gpu_frame;
-    video_reader->nextFrame(gpu_frame);
-    gpu_frame.download(frame);
-#else
-    cv::VideoCapture video_reader(video_path.string());
-    video_reader >> frame;
-#endif
-    cv::VideoWriter video_writer(output_path.string(), cv::VideoWriter::fourcc('I', '4', '4', '4'), get_fps(video_path), frame.size());
-    while (true) {
-#ifdef HAVE_OPENCV_CUDACODEC
-        if (!video_reader->nextFrame(gpu_frame))
-            break;
-        gpu_frame.download(frame);
-#else
-        video_reader >> frame;
-        if (frame.empty())
-            break;
-#endif
-        frame = make_noise(frame, SNR);
-        video_writer << frame;
+        video_writer = cv::VideoWriter(this->output_path.string(), cv::VideoWriter::fourcc('I', '4', '4', '4'), get_fps(this->filename), frame.size());
     }
-    video_reader.release();
-    video_writer.release();
-}
+
+    ~Noise_Video_Writer() {
+        video_reader.release();
+        video_writer.release();
+    }
+
+    void frame_operation() {
+        noise_frame = make_noise(frame, this->SNR);
+        video_writer << noise_frame;
+    }
+
+private:
+    std::filesystem::path output_path;
+    double SNR;
+    cv::VideoWriter video_writer;
+    cv::Mat noise_frame;
+};
 
 void thread_invoker(int deviceID) {
 #ifdef HAVE_OPENCV_CUDACODEC
@@ -89,18 +84,21 @@ void thread_invoker(int deviceID) {
         std::filesystem::path filename = working_queue.front();
         working_queue.pop();
         queue_mutex.unlock();
-        create_video(filename, SNR);
+        Noise_Video_Writer writer(filename, SNR);
+        writer.run();
+        writer.print_performance();
     }
 }
 
 int main(int argc, char** argv) {
     if (argc < 3 || argc > 4) {
-        // TODO add help info
-        return -1;
+        cin >> output_dir >> SNR >> seed_prefix;
     }
-    output_dir = argv[1];
-    SNR = std::stod(argv[2]);
-    seed_prefix = argc == 4 ? argv[3] : "0";
+    else {
+        output_dir = argv[1];
+        SNR = std::stod(argv[2]);
+        seed_prefix = argc == 4 ? argv[3] : "0";
+    }
 
     cv::setNumThreads(0);
     read_config();
