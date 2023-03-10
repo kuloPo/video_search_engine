fc3a366476268ea2fe66fba101076b6b85e7018c
クロポ <reg@kulopo.com>
2023-03-10 22:45:14 +0800

    add cmake file

--- targets/test_video_decode.cpp
+++ targets/test_video_decode.cpp
@@ -28,24 +28,11 @@ public:
 	}
 
 	virtual void frame_operation() {
-		frame_preprocessing(frame);
-		noise_frame = make_noise(frame, 1);
-		edge_detection(noise_frame, edge_frame);
-		//cv::GaussianBlur(noise_frame, edge_frame, cv::Size(7, 7), 3, 0, cv::BORDER_DEFAULT);
-		//cv::Mat sobel_x, sobel_y;
-		//cv::Mat kernel_x = (cv::Mat_<double>(3, 3) << -1, 0, 1, -2, 0, 2, -1, 0, 1);
-		//cv::Mat kernel_y = (cv::Mat_<double>(3, 3) << 1, 2, 1, 0, 0, 0, -1, -2, -1);
-		//cv::filter2D(edge_frame, sobel_x, -1, kernel_x, cv::Point(-1, -1));
-		//cv::filter2D(edge_frame, sobel_y, -1, kernel_y, cv::Point(-1, -1));
-		//cv::pow(sobel_x, 2, sobel_x);
-		//cv::pow(sobel_y, 2, sobel_y);
-		//cv::sqrt((sobel_x + sobel_y), edge_frame);
-		cv::imshow("", edge_frame * 5);
+		cv::imshow("", frame);
 		cv::waitKey(fps);
 	}
 private:
 	int fps;
-	cv::Mat noise_frame, edge_frame;
 };
 
 int main() {
